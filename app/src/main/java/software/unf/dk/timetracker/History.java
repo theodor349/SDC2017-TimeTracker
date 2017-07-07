2c32b73152c1913758786518389e483b73afe8ad
deltager <github@software.unf.dk>
2017-07-07 11:42:04 +0200

    Update 1.3 Pie Chart made

--- app/src/main/java/software/unf/dk/timetracker/History.java
+++ app/src/main/java/software/unf/dk/timetracker/History.java
@@ -1,22 +1,28 @@
 package software.unf.dk.timetracker;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 //import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.charts.LineChart;
+//import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class History extends AppCompatActivity {
 
-    private int amountOfTenis = 10;
-    private int amountTotal = 100;
+    private int amountOfTennis = 100;
+    private int amountSoccer = 200;
+    private int amountSwimming = 50;
+    private int amountBadminton = 90;
     private PieChart pieChart;
 
     @Override
@@ -28,14 +34,28 @@ public class History extends AppCompatActivity {
 
         List<PieEntry> entries = new ArrayList<>();
 
-        entries.add(new PieEntry(amountOfTenis, "Tennis"));
-        entries.add(new PieEntry(amountTotal, "Total"));
+        entries.add(new PieEntry(amountOfTennis, "Tennis"));
+        entries.add(new PieEntry(amountSoccer, "Soccer"));
+        entries.add(new PieEntry(amountSwimming, "Swimming"));
+        entries.add(new PieEntry(amountBadminton, "Badminton"));
 
-        PieDataSet set = new PieDataSet(entries, "Sport");
-        PieData data = new PieData(set);
+        inToChart(amountOfTennis,entries,"Tennis");
+        inToChart(amountSoccer,entries,"Soccer");
+        inToChart(amountOfTennis,entries,"Tennis");
+        inToChart(amountOfTennis,entries,"Tennis");
+        inToChart(amountOfTennis,entries,"Tennis");
+
+        PieDataSet dam = new PieDataSet(entries, "Sport");
+        PieData data = new PieData(dam);
+        dam.setColors(new int[] { R.color.neonpink, R.color.green, R.color.blue, R.color.lightgreen, R.color.red, R.color.yellow, R.color.lightblue, R.color.magenza, R.color.orange, R.color.turqoise, R.color.pumpkin, R.color.palepink, R.color.svump, R.color.darkpurple }, getApplicationContext());
         pieChart.setData(data);
         pieChart.invalidate(); // refresh
 
+
+    }
+
+    private void inToChart (int amount, List list, String cataName){
+        list.add(new PieEntry(amount, cataName));
 
     }
 }
